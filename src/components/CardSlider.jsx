975a6df536f3a194d372dc5fe80d2aa28e9ca4ad
Jabulani Sifundza <jjsifundza@gmail.com>
2022-09-24 22:28:23 +0200

    Slider fix

--- src/components/CardSlider.jsx
+++ src/components/CardSlider.jsx
@@ -1,11 +1,16 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useRef} from 'react';
 import Card from './Card';
 import styled from "styled-components";
+import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 
 export default function CardSlider({data, title}){
 	const [showControls, setShowControls] = useState(false);
 	const listRef = useRef();
 	const [sliderPosition, setSliderPosition] = useState(0);
+
+	const handleDirection = (direction)=>{
+
+	}
 
 	return (
 	<Container className="flex column"
@@ -13,21 +18,26 @@ export default function CardSlider({data, title}){
 	onMouseLeave={()=> setShowControls(false)}>
 		<h1>{title}</h1>
 		<div className="wrapper">
-			<div className={`slider-action left ${!showControls ? "none" : ""}`}>
+			<div className={`slider-action left ${!showControls ? "none" : ""} flex j-center a-center`}>
+			<AiOutlineLeft onClick={()=> handleDirection("left")}/>
+			</div>	
 			
-			</div>		
-		</div>
-
-		<div className="flex">
+			
+		<div className="flex slider" ref={listRef}>
 		{
 			data.map((movie, index)=>{
 				return <Card movieData={movie} index={index} key={movie.id}/>
 			})
 		}
 		</div>
-	
+		<div className={`slider-action right ${!showControls ? "none" : ""} flex j-center a-center`}>
+		<AiOutlineRight onClick={()=> handleDirection("right")}/>
+		</div>
 
-		
+		</div>
 	</Container>
 	);
 }
+
+const Container = styled.div`
+`
